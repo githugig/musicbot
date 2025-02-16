f89c572d63dd7152b11642ab3148fa46e7dc8635
githugig <marcelmatys95@gmail.com>
2025-02-16 14:50:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://marcelmatys:<GoGGyGoGGy322>@cluster0.nf7ou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
